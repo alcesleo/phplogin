3804eb61ccb1f19d0ac295c7e0fac88b9c656981
alcesleo <lagginglion@gmail.com>
2013-09-11 10:56:36 +0200

    A bunch of testing code that for now seems to work

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -8,6 +8,7 @@ use models\UserModel;
 use views\LoginView;
 use views\DateTimeView;
 use views\UserView;
+use views\AppView;
 
 /**
  * # UC1 Autentisera användare
@@ -31,34 +32,33 @@ use views\UserView;
 class LoginController
 {
 
+    private $loginModel;
+
     // Views
     private $loginView;
     private $dateTimeView;
+    private $appView;
 
+    // TODO: Send in userdatabase
     public function __construct()
     {
+        $this->loginModel = new LoginModel();
+
         // Create instances
-        $this->loginView = new LoginView();
+        $this->loginView = new LoginView($this->loginModel);
         $this->dateTimeView = new DateTimeView('sv_SE', '%A, den %e %B år %Y. Klockan är [%H:%M:%S].');
+
+        // TODO: Should this be passed in as a param?
+        $this->appView = new AppView();
     }
 
     public function logIn()
     {
-        $this->handleInput();
+        $loginHTML;
+        $dateHTML;
 
-        $output = '';
-
-        $output .= $this->loginView->getHTML();
-
-        $output .= $this->dateTimeView->getHTML();
-
-        return $output;
-    }
-
-    private function handleInput()
-    {
         // When ?login is hit
-        if ($this->loginView->userWantsToLogIn()) {
+        if ($this->appView->userWantsToLogIn()) {
 
             // Validate form
             if ($this->loginView->validateFormInput()) {
@@ -66,13 +66,23 @@ class LoginController
                 // Authorize user
                 try {
                     $user = UserModel::authorizeUser($this->loginView->getPostUserName(), $this->loginView->getPostPassword());
+                    // TODO: Unfuck this up.
+
+                    $this->loginView->showLoginSuccess($user);
+                    $loginHTML = $this->loginView->getWelcomeHTML();
+                    $this->loginView->setSessionCredentials($user);
                 } catch (\Exception $ex) {
-                    $this->loginView->showFailedLogin();
+                    $loginHTML = $this->loginView->showLoginFailed();
                 }
+            } else {
+                $loginHTML = $this->loginView->getFormHTML();
             }
-
-
+        } else {
+            $loginHTML = $this->loginView->getFormHTML();
         }
-    }
 
+        $dateHTML = $this->dateTimeView->getHTML();
+        print $this->appView->getHTML($loginHTML, $dateHTML);
+
+    }
 }
