8f6e9cb255257b3eb3c51f649734797fbab92454
alcesleo <lagginglion@gmail.com>
2013-09-18 16:19:56 +0200

    Added UserCredentialsModel, no logic yet but usable

---
+++ app/src/Phplogin/Models/UserCredentialsModel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Models;
+
+/**
+ * Contains rules of how usernames and passwords are formed
+ */
+class UserCredentialsModel
+{
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * Creates an object containing properly formed credentials
+     * @param string $username
+     * @param string $password
+     */
+    public function __construct($username, $password)
+    {
+        // TODO: Validate forming of strings
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
