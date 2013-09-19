b913ddfab7cb6245bc35c332e3eaa1d082353a51
alcesleo <lagginglion@gmail.com>
2013-09-19 17:04:53 +0200

    Finally figured out what this was for, and now it even works!

--- app/src/Phplogin/Models/LoginModel.php
+++ app/src/Phplogin/Models/LoginModel.php
@@ -2,15 +2,106 @@
 
 namespace Phplogin\Models;
 
-use Phplogin\Models\UserDatabaseModel as UserDB;
+use Phplogin\Models\UserModel;
+use Phplogin\Models\UserListModel;
+use Phplogin\Models\UserCredentialsModel;
+use Exception;
+use Phplogin\Exceptions\NotFoundException;
 
 class LoginModel
 {
-    public function isLoggedIn()
+    // TODO: Order functions
+
+    private $db;
+
+    private static $sessionLoggedIn = 'LoginModel::LoggedInUser';
+
+    public function __construct(UserListModel $database)
     {
+        $this->db = $database;
     }
 
-    public function logInUser(UserCredentials $credentials) {}
+    /**
+     * Log in a user
+     * @param  UserCredentialsModel $credentials
+     * @return UserModel // REALLY???
+     * @throws Exception If not authorized
+     * @throws NotFoundException If user doesn't exist
+     */
+    public function logIn(UserCredentialsModel $credentials)
+    {
+        // TODO: Authorize credentials
 
-    public function logOut($user) {}
+        // Get user from database
+        // NOTE: This can throw NotFoundException
+        $user = $this->db->getUserByName($credentials->getUsername());
+
+        // Authorize
+        if (! $this->authorize($credentials, $user)) {
+            throw new Exception('Not authorized');
+        }
+
+        $this->persistLogin($user);
+
+        return $user;
+    }
+
+    public function getLoggedInUser()
+    {
+        if (! $this->isLoggedIn()) {
+            throw new Exception('No user logged in');
+        }
+        return unserialize($_SESSION[self::$sessionLoggedIn]);
+    }
+
+    /**
+     * Save the user session
+     * @param  UserModel $user
+     */
+    private function persistLogin(UserModel $user)
+    {
+        // TODO: Check for session theft
+        $_SESSION[self::$sessionLoggedIn] = serialize($user);
+    }
+
+    private function authorize(UserCredentialsModel $credentials, UserModel $user)
+    {
+        if ($credentials->getUsername() != $user->getUsername()) {
+            return false;
+        }
+
+        // FIXME: new Password
+        /*
+        $dbPass = $user->getPassword();
+        if ($dbPass->matchCredentials($credentials))
+        */
+        // FIXME: Don't encrypt here
+        if (sha1($credentials->getPassword()) != $user->getHash()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function logOut()
+    {
+        // TODO: Delete session variables
+        unset($_SESSION[self::$sessionLoggedIn]);
+    }
+
+    /**
+     * If this session is logged in
+     * @return bool
+     */
+    public function isLoggedIn()
+    {
+        // TODO: May need a closer look...
+        return isset($_SESSION[self::$sessionLoggedIn]);
+    }
+
+    // TODO: Preliminary, return session|cookie|input?
+    public function loggedInBy()
+    {
+
+    }
 }
