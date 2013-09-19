a3b236753ba50300166e479159f52caa16c2d721
alcesleo <lagginglion@gmail.com>
2013-09-19 17:06:59 +0200

    Some fine-tuning

--- app/src/Phplogin/Views/AppView.php
+++ app/src/Phplogin/Views/AppView.php
@@ -7,12 +7,28 @@ use Phplogin\Views\DateTimeView;
 
 class AppView
 {
+    // TODO: How do getFormHTML know about these?
     private static $loginPage = 'login';
     private static $logoutPage = 'logout';
 
-    public function getHTML($loginHTML, $dateHTML)
+    /**
+     * @var DateTimeView
+     */
+    private $dateTimeView;
+
+    public function __construct()
     {
-        $title = 'PHPLogin';
+        $this->dateTimeView = new DateTimeView('sv_SE.UTF-8', '%A, den %e %B år %Y. Klockan är [%H:%M:%S].');
+    }
+
+    /**
+     * Generate the complete html-page
+     * @param string $title title of the page
+     * @param string $body HTML
+     */
+    public function getHTML($body, $title = 'PHPLogin')
+    {
+        $dateHTML = $this->dateTimeView->getHTML();
 
         return "<!DOCTYPE html>
             <html>
@@ -22,18 +38,26 @@ class AppView
             </head>
             <body>
                 <h1>PHPLogin</h1>
-                $loginHTML
+                $body
                 $dateHTML
             </body>
             </html>
         ";
     }
 
+    /**
+     * If loginpage is active
+     * @return bool
+     */
     public function userWantsToLogIn()
     {
         return isset($_GET[self::$loginPage]);
     }
 
+    /**
+     * If logoutpage is active
+     * @return bool
+     */
     public function userWantsToLogOut()
     {
         return isset($_GET[self::$logoutPage]);
