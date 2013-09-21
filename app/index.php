fb706dcd5d0b878e180fd4cbb714908cea847c3d
alcesleo <lagginglion@gmail.com>
2013-09-21 18:25:19 +0200

    Code for adding Admin user

--- app/index.php
+++ app/index.php
@@ -8,6 +8,10 @@ require_once('./vendor/autoload.php');
 
 use Phplogin\Controllers\LoginController;
 
+// Create the Admin-user
+//$db = new UserListModel('db/users.sqlite');
+//$db->insertUser(new UserModel('Admin', sha1('Password')));
+
 // Fire it up!
 $loginCtrl = new LoginController();
 $loginCtrl->printLoginPage();
