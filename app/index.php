2795dd1ba5adae932b63f20f1ab273105fd43fed
alcesleo <lagginglion@gmail.com>
2013-09-23 14:17:04 +0200

    Removed todo:s

--- app/index.php
+++ app/index.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
 
-// FIXME: All files are missing docblocks
-// TODO: Use short version of return types in docblocs, e.g. integer -> int
-
 require_once('./vendor/autoload.php');
 
 use Phplogin\Controllers\LoginController;
